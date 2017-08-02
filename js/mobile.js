8a6c4dcb151a531597217910499af422385c0580
Vladislav <drimacus182@gmail.com>
2017-08-02 16:43:29 +0300

    прототип тексту справа

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,6 @@
 (function () {
-    if(screen.width <= 767 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    if(screen.width <= 767) {
+        // || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         window.__this_is_mobile__ = true;
     }
 })();
